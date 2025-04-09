a7de4873dbb1b5286c0cdbda18e3e217e3e0f922
e-kif <e.kifiuk@gmail.com>
2025-04-09 10:47:50 +0200

    app todo list

---
+++ app.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+
+    # todo flask routers with blueprints
+    # todo swagger docs
+    # todo crud operations
+    # todo pagination
+    # todo sorting
+    # todo filtering
+    # todo database models
+    # todo pydantic schemas
+    # todo postgres database init
+    # todo user list + database counterpart
+    # todo jwt auth
+    # todo test coverage
+    # todo update readme
+    # todo deploy
+    # todo ci/cd workflow
