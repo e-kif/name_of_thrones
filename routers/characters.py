d3bd4da2c0d4f13c16cc41b47cdfc21256f4853e
e-kif <e.kifiuk@gmail.com>
2025-04-09 19:24:41 +0200

    character delete and update routes init

--- routers/characters.py
+++ routers/characters.py
@@ -4,18 +4,34 @@ from schemas.characters import CharacterInOpt, CharacterOut
 
 characters_bp = Blueprint('character', __name__)
 
+db = json_db()
+
 
 @characters_bp.route('/', methods=['GET'])
 def get_characters():
-    return json_db().characters, 200
+    return db.characters, 200
 
 
 @characters_bp.route('/<int:character_id>', methods=['GET'])
-def get_character(character_id):
-    return json_db().read_character(character_id), 200
+def get_character(character_id: int) -> CharacterOut:
+    return db.read_character(character_id), 200
 
 
 @characters_bp.route('/', methods=['POST'])
 def add_character() -> CharacterOut:
     new_character = request.get_json()
-    return json_db().add_character(new_character), 201
+    return db.add_character(new_character), 201
+
+
+@characters_bp.route('/<int:charter_id>', methods=['DELETE'])
+def remove_character(character_id: int) -> CharacterOut:
+    removed_character = db.remove_character(character_id)
+    return removed_character
+
+
+@characters_bp.route('/<int:character_id>', methods=['PUT'])
+def update_character(character_id: int) -> CharacterOut:
+    updated_character = request.get_json()
+    db_character = db.update_character(updated_character)
+    return db_character
+    
