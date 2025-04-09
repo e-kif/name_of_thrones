9cfe1826639d699328c4dc42534ca7976e43c5df
e-kif <e.kifiuk@gmail.com>
2025-04-09 11:51:27 +0200

    json data manager integration tests

--- data/json_data_manager.py
+++ data/json_data_manager.py
@@ -1,6 +1,6 @@
 import os
 import json
-from data_manager import DataManager
+from data.data_manager import DataManager
 
 
 class JSONDataManager(DataManager):
@@ -8,12 +8,12 @@ class JSONDataManager(DataManager):
 
     def __init__(
             self,
-            storage_file: str = os.path.join(['storage', 'characters.json'])
+            storage_file: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'storage', 'characters.json')
             ):
         """Constructor method loads data from json file
         into self.storage instance variable
         """
-        self.storage = self.load_json_file()
+        self.storage = self.load_json_file(storage_file)
 
     def load_json_file(self, filename: str) -> list | dict:
         """Loads info from JSON file as a Python object (list or dictionary)"""
@@ -23,6 +23,24 @@ class JSONDataManager(DataManager):
     def read_characters(self):
         """Returns current state of the instance storage"""
         return self.storage
+
+    def add_character(self, character):
+        """Adds new character to the instance storage"""
+        return super().add_character(character)
+
+    def read_character(self, character_id):
+        """Reads a character with id=character_id from the instance storage"""
+        return super().read_character(character_id)
+
+    def remove_character(self, character_id):
+        """Deletes a character with id=character_id
+        from the instance storage
+        """
+        return super().remove_character(character_id)
+
+    def update_character(self, character_id, character):
+        """Updates character info for the caracter with id=character_id"""
+        return super().update_character(character_id, character)
 
     @property
     def characters(self):
---
+++ test/test_integration.py
@@ -0,0 +1,11 @@
+from data.json_data_manager import JSONDataManager as json_db
+
+
+def test_json_db_integration():
+    assert json_db().__getattribute__('__init__'), 'Constructor method is missing'
+    assert json_db().__getattribute__('load_json_file'), 'Load json method is missing'
+    assert json_db().__getattribute__('read_characters'), 'Read characters method is missing'
+    assert json_db().__getattribute__('read_character'), 'Read character method is missing'
+    assert json_db().__getattribute__('add_character'), 'Add character method is missing'
+    assert json_db().__getattribute__('update_character'), 'Update character method is missing'
+    assert json_db().__getattribute__('characters'), 'Characters getter method is missing'
