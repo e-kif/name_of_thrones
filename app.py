1516029b88ef21ebd4bd8ae8717359ea2d7e760b
e-kif <e.kifiuk@gmail.com>
2025-04-10 22:58:22 +0200

    app creation refactor for db swap for tests

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 from routers.characters import characters_bp
+from data.json_data_manager import JSONDataManager
 
 
-def create_app():
+def create_app(db_path: str = os.path.join('storage', 'characters.json')):
     """Creates the app, registers all blueprints, return the app"""
     app = Flask(__name__)
+    app.data_manager = JSONDataManager(db_path)
     app.register_blueprint(characters_bp, url_prefix='/characters')
     return app
 
--- test/conftest.py
+++ test/conftest.py
@@ -1,61 +1,72 @@
+import os
 import pytest
+import tempfile
+import json
 from app import create_app
 
 
-@pytest.fixture(scope="session")
-def app():
-    """Session-wide test 'app' fixture."""
-    app = create_app()
-    app.config.update({'TESTING': True})
-    yield app
+@pytest.fixture(scope='function')
+def client():
+    """Test client setup with temporary database file"""
+    db_fd, db_path = tempfile.mkstemp()
 
+    with open(os.path.join('storage', 'characters.json'), 'r', encoding='utf8') as original:
+        with open(db_path, 'w', encoding='utf8') as f:
+            f.write(json.dumps(json.loads(original.read())))
 
-@pytest.fixture
-def client(app):
-    """Test client for the application"""
-    return app.test_client()
+    app = create_app(db_path)
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
+    with open(db_path, 'r') as f:
+        print(f.read())
+
+    os.close(db_fd)
+    os.remove(db_path)
 
 
 @pytest.fixture()
 def jon_snow():
-    return {"age": 25,
-            "animal": "Direwolf",
-            "death": None,
-            "house": "Stark",
-            "id": 1,
-            "name": "Jon Snow",
-            "nickname": "King in the North",
-            "role": "King",
-            "strength": "Physically strong",
-            "symbol": "Wolf"}
+    return {'age': 25,
+            'animal': 'Direwolf',
+            'death': None,
+            'house': 'Stark',
+            'id': 1,
+            'name': 'Jon Snow',
+            'nickname': 'King in the North',
+            'role': 'King',
+            'strength': 'Physically strong',
+            'symbol': 'Wolf'}
 
 
 @pytest.fixture()
 def daenerys():
-    return {"age": 24,
-            "animal": "Dragon",
-            "death": 8,
-            "house": "Targaryen",
-            "id": 2,
-            "name": "Daenerys Targaryen",
-            "nickname": "Mother of Dragons",
-            "role": "Queen",
-            "strength": "Cunning",
-            "symbol": "Dragon"}
+    return {'age': 24,
+            'animal': 'Dragon',
+            'death': 8,
+            'house': 'Targaryen',
+            'id': 2,
+            'name': 'Daenerys Targaryen',
+            'nickname': 'Mother of Dragons',
+            'role': 'Queen',
+            'strength': 'Cunning',
+            'symbol': 'Dragon'}
 
 
 @pytest.fixture()
 def olenna_tyrell():
-    return {"age": 70,
-            "animal": None,
-            "death": 7,
-            "house": "Tyrell",
-            "id": 50,
-            "name": "Olenna Tyrell",
-            "nickname": "Queen of Thorns",
-            "role": "Matriarch",
-            "strength": "Cunning",
-            "symbol": "Rose"}
+    return {'age': 70,
+            'animal': None,
+            'death': 7,
+            'house': 'Tyrell',
+            'id': 50,
+            'name': 'Olenna Tyrell',
+            'nickname': 'Queen of Thorns',
+            'role': 'Matriarch',
+            'strength': 'Cunning',
+            'symbol': 'Rose'}
 
 
 @pytest.fixture()
