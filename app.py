823f709267d050bf432ec74d733788a0c6f5c3af
e-kif <e.kifiuk@gmail.com>
2025-04-17 07:38:21 +0200

    switch to postgres database

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
 from routers.characters import characters_bp
 from data.json_data_manager import JSONDataManager
+from data.sql_data_manager import SQLDataManager
 
 
 def create_app(db_path: str = os.path.join('storage', 'characters.json')):
     """Creates the app, registers all blueprints, return the app"""
+    load_dotenv()
     app = Flask(__name__)
-    app.data_manager = JSONDataManager(db_path)
+    # app.data_manager = JSONDataManager(db_path)
+    app.data_manager = SQLDataManager(os.getenv('DATABASE_URI'))
     app.register_blueprint(characters_bp, url_prefix='/characters')
     return app
 
@@ -22,9 +26,7 @@ if __name__ == '__main__':
     main()
 
     # todo swagger docs
-    # todo database models
     # todo pydantic schemas
-    # todo postgres database init
     # todo user list + database counterpart
     # todo jwt auth
     # todo test coverage
