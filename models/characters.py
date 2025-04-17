b95a9ff9633d6fb013c7eb5af870724460be6710
e-kif <e.kifiuk@gmail.com>
2025-04-17 23:07:41 +0200

    db models defined with dynamic hybrid properties

--- models/characters.py
+++ models/characters.py
@@ -1,11 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, object_session
 from sqlalchemy.ext.hybrid import hybrid_property
+from flask import current_app
 
 
 class Base(DeclarativeBase):
     pass
-    
+
 
 db = SQLAlchemy(model_class=Base)
 
@@ -13,22 +14,108 @@ character_house = db.Table(
     'character_house',
     db.Column('character_id', db.Integer, db.ForeignKey('characters.id'), primary_key=True),
     db.Column('house_id', db.Integer, db.ForeignKey('houses.id'), nullable=False),
-    db.UniqueConstraint('character_id', name='unique_character_constraint')
+    db.UniqueConstraint('character_id', name='unique_character_constraint_house')
     )
 
 character_symbol = db.Table(
     'character_symbol',
     db.Column('character_id', db.Integer, db.ForeignKey('characters.id'), primary_key=True),
     db.Column('symbol_id', db.Integer, db.ForeignKey('symbols.id'), nullable=False),
-    db.UniqueConstraint('character_id', name='unique_character_constraint')
+    db.UniqueConstraint('character_id', name='unique_character_constraint_symbol')
     )
 
 character_animal = db.Table(
     'character_animal',
     db.Column('character_id', db.Integer, db.ForeignKey('characters.id'), primary_key=True),
     db.Column('animal_id', db.Integer, db.ForeignKey('animals.id'), nullable=False),
-    db.UniqueConstraint('character_id', name='unique_character_constraint')
+    db.UniqueConstraint('character_id', name='unique_character_constraint_animal')
     )
+
+
+def dynamic_hybrid_property(property_name, attribute_name, model_class, always_create_new_record: bool = False):
+    """Dynamically creates a hybrid property for related entities"""
+
+    def field_getter(self):
+        relation = getattr(self, attribute_name)
+        return getattr(relation, property_name) if relation else None
+    
+    def field_setter(self, value):
+        relation = getattr(self, attribute_name)
+
+        def resolve(val):
+            if isinstance(val, model_class):
+                return val
+            
+            if always_create_new_record:
+                return model_class(**{property_name: val})
+            item = db.session.query(model_class).filter(getattr(model_class, property_name) == val).first()
+            if not item:
+                item = model_class(**{property_name: val})
+                db.session.add(item)
+            return item
+        
+        if value is None:
+            relation.clear()
+        else:
+            setattr(self, attribute_name, resolve(value))
+
+    def field_expression(cls):
+        relation = getattr(cls, attribute_name)
+        return getattr(model_class, property_name)
+    
+    return hybrid_property(fget=field_getter, fset=field_setter).expression(field_expression)
+    
+
+class Age(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    age: Mapped[int] = mapped_column(nullable=False)
+    character_id: Mapped[int] = mapped_column(db.ForeignKey('characters.id'), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'{self.character.name} is {self.age} years old'
+
+
+class Death(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    death: Mapped[int] = mapped_column(nullable=False)
+    character_id: Mapped[int] = mapped_column(db.ForeignKey('characters.id'), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'{self.character.name} dead in season {self.death}'
+
+
+class Houses(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'House {self.name} ({self.id})'
+
+
+class Symbols(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+    def __repr__(self):
+        return f'Symbol {self.name} ({self.id})'
+
+
+class Animals(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'Animal {self.name} ({self.id})'
+
+
+class Nicknames(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    character_id: Mapped[int] = mapped_column(db.ForeignKey('characters.id'), nullable=False)
+
+    def __repr__(self):
+        return f'{self.character.name} AKA {self.name} ({self.id})'
+    
 
 class Characters(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -36,61 +123,34 @@ class Characters(db.Model):
     role: Mapped[str] = mapped_column(nullable=False)
     strength: Mapped[str] = mapped_column(nullable=False)
 
-    ages = db.relationship('Age', backref='character')
     houses = db.relationship('Houses', secondary=character_house, uselist=False, backref='residents')
     nicknames = db.relationship('Nicknames', uselist=False, backref='characters')
     symbols = db.relationship('Symbols', secondary=character_symbol, uselist=False, backref='characters')
     animals = db.relationship('Animals', secondary=character_animal, uselist=False, backref='characters')
+    ages = db.relationship('Age', uselist=False, backref='character')
+    deaths = db.relationship('Death', uselist=False, backref='character')
+
+    house = dynamic_hybrid_property('name', 'houses', Houses)
+    nickname = dynamic_hybrid_property('name', 'nicknames', Nicknames, always_create_new_record=True)
+    symbol = dynamic_hybrid_property('name', 'symbols', Symbols)
+    animal = dynamic_hybrid_property('name', 'animals', Animals)
+    age = dynamic_hybrid_property('age', 'ages', Age, always_create_new_record=True)
+    death = dynamic_hybrid_property('death', 'deaths', Death, always_create_new_record=True)
     
-    @hybrid_property
-    def age(self):
-        return self.ages[0].age if self.ages else None
+    @property
+    def dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'house': self.house,
+            'age': self.age,
+            'role': self.role,
+            'animal': self.animal,
+            'symbol': self.symbol,
+            'death': self.death,
+            'strength': self.strength,
+            'nickname': self.nickname
+        }
     
-    @hybrid_property
-    def house(self):
-        return self.houses.name if self.houses else None
-    
-    @hybrid_property
-    def nickname(self):
-        return self.nicknames.name if self.nicknames else None
-    
-    @hybrid_property
-    def symbol(self):
-        return self.symbols.name if self.symbols else None
-    
-    @hybrid_property
-    def animal(self):
-        return self.animals.name if self.animals else None
-
-
-class Age(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    age: Mapped[int] = mapped_column(nullable=False)
-    character_id: Mapped[int] = mapped_column(db.ForeignKey('characters.id'), nullable=False)
-
-
-class Death(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    death: Mapped[int] = mapped_column(nullable=False)
-    character_id: Mapped[int] = mapped_column(db.ForeignKey('characters.id'), nullable=False)
-
-
-class Houses(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True, nullable=False)
-
-
-class Symbols(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True)
-
-
-class Animals(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True, nullable=False)
-
-
-class Nicknames(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False, unique=True)
-    character_id: Mapped[int] = mapped_column(db.ForeignKey('characters.id'), nullable=False)
+    def __repr__(self):
+        return f'{self.id}. {self.name} (age {self.age}, death {self.death}, house {self.house})'
